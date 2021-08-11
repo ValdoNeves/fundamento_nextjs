05b87070366ce177d0f801654962b3c15355cbe6
ValdoNeves <valdogt1@gmail.com>
2021-08-11 00:57:11 -0300

    function padrao e function async

--- src/pages/integracao_1.jsx
+++ src/pages/integracao_1.jsx
@@ -3,12 +3,19 @@ import Layout from "../components/Layout"
 
 export default function Integracao() {
 
+  const [codigo, setCodigo] = useState(1)
   const [cliente, setCliente] = useState({})
 
-  function obterCliente(){
-    fetch('http://localhost:3000/api/clientes/123')
+  function obterCliente() {
+    fetch(`http://localhost:3000/api/clientes/${codigo}`)
       .then(resp => resp.json())
       .then(dados => setCliente(dados))
+  }
+
+  async function obterClienteAsync() {
+    const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`)
+    const dados = await resp.json()
+    setCliente(dados)
   }
 
   return (
@@ -19,8 +26,13 @@ export default function Integracao() {
         <li>email: {cliente.email} </li>
       </ul>
       <div>
-      <input type="number"  />
-      <button onClick={obterCliente}>Obter cliente</button>
+        <input type="number" value={codigo} onChange={e => setCodigo(e.target.value)} />
+        <div>
+          <button onClick={obterCliente}>Obter cliente</button>
+        </div>
+        <div>
+          <button onClick={obterClienteAsync}>Obter cliente asyncrono</button>
+        </div>
       </div>
 
     </Layout>
